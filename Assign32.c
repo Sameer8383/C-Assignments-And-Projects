15d4feb9b65293ff3f0c8aa7593648a53b4bb4c9
C-Assignments-And-projects <sameersharma8383@gmail.com>
2025-12-09 13:41:41 +0530

    32nd Assignment of C

--- Assign32.c
+++ Assign32.c
@@ -1,274 +1,340 @@
 #include <stdio.h>
 
-int length(char *ptr)
+// ================================================================
+// Assignment 32: Arrays and Functions (Normal & Pointer Function Calls)
+// ================================================================
+
+// -------------------- Q1: Swap two elements --------------------
+int swapIndex(int *ptr, int index1, int index2)
 {
-    int count = 0;
-    for (int i = 0; ptr[i] != '\0'; i++)
-    {
-        if (ptr[i] == '\n')
-        {
-            ptr[i] = '\0';
-            break;
-        }
-        if (ptr[i] != ' ')
-            count++;
-    }
-    return count;
+  int temp;
+  temp = *(ptr + index1);
+  *(ptr + index1) = *(ptr + index2);
+  *(ptr + index2) = temp;
+  return temp;
 }
 
 void f1()
 {
-    printf("===================================================================================\n");
-    printf(" Q1. Find length of a string (Normal Function Call)\n");
-    printf("===================================================================================\n\n");
+  printf("\n*===================== Using Normal Function Call =====================*\n");
+  printf("Q1: Swap two elements of a given array with specified indices\n\n");
 
-    char str[100] = "Hello World";
-    char *ptr = str;
+  int arr[10] = {1, 7, 4, 5, 6, 3, 2, 9, 8, 0};
+  int *ptr = arr;
+  int index1 = 3, index2 = 7;
 
-    printf("String: \"%s\"\n", str);
-    int len = length(ptr);
-    printf("Length of string (excluding spaces): %d\n", len);
+  printf("Before Swapping: ");
+  for (int i = 0; i < 10; i++)
+    printf("%d ", *(arr + i));
 
-    printf("\n===================================================================================\n");
-    printf("                              END OF PROGRAM 1                                        \n");
-    printf("===================================================================================\n\n\n");
+  swapIndex(ptr, index1, index2);
+
+  printf("\nAfter Swapping:  ");
+  for (int i = 0; i < 10; i++)
+    printf("%d ", *(arr + i));
+
+  printf("\n\n*----------------------------- END OF Q1 -----------------------------*\n\n\n");
 }
 
 void f1_()
 {
-    printf("===================================================================================\n");
-    printf(" Q1.1 Find length of a string (Pointer Function Call)\n");
-    printf("===================================================================================\n\n");
+  printf("\n*===================== Using Pointer Function Call ===================*\n");
+  printf("Q1.1: Swap two elements using function pointer\n\n");
 
-    char str[100] = "Hello World";
-    char *ptr = str;
+  int arr[10] = {1, 7, 4, 5, 6, 3, 2, 9, 8, 0};
+  int *ptr = arr;
+  int index1 = 2, index2 = 5;
 
-    int (*fptr)(char *) = &length;
-    int len = fptr(ptr);
+  printf("Before Swapping: ");
+  for (int i = 0; i < 10; i++)
+    printf("%d ", *(arr + i));
 
-    printf("String: \"%s\"\n", str);
-    printf("Length of string (excluding spaces): %d\n", len);
+  int (*fptr)(int *, int, int) = swapIndex;
+  fptr(ptr, index1, index2);
 
-    printf("\n===================================================================================\n");
-    printf("                              END OF PROGRAM 1.1                                      \n");
-    printf("===================================================================================\n\n\n");
+  printf("\nAfter Swapping:  ");
+  for (int i = 0; i < 10; i++)
+    printf("%d ", *(arr + i));
+
+  printf("\n\n*---------------------------- END OF Q1.1 ----------------------------*\n\n\n");
 }
 
-int occurrences(char *ptr, char ch)
+// -------------------- Utility: Sort Array --------------------
+int shortArray(int *ptr, int size)
 {
+  int temp;
+  for (int round = 1; round < size; round++)
+  {
+    for (int i = 0; i < size - round; i++)
+    {
+      if (*(ptr + i) > *(ptr + i + 1))
+      {
+        temp = *(ptr + i);
+        *(ptr + i) = *(ptr + i + 1);
+        *(ptr + i + 1) = temp;
+      }
+    }
+  }
+  return temp;
+}
+
+// -------------------- Q2: Count Duplicates --------------------
+void countDuplicate(int *ptr, int size)
+{
+  printf("\nElements occurring 2 or more times:\n");
+  printf("-------------------------------\n");
+  printf("Element  Occurs\n");
+  printf("-------  ------\n");
+
+  for (int i = 0; i < size; i++)
+  {
     int count = 0;
-    for (int i = 0; ptr[i] != '\0'; i++)
-        if (ptr[i] == ch)
-            count++;
-    return count;
+    for (int j = i + 1; j < size; j++)
+    {
+      if (*(ptr + i) == *(ptr + j))
+        count++;
+      else
+        break;
+    }
+    if (count >= 1)
+    {
+      printf("%d\t  %d times\n", *(ptr + i), count + 1);
+      i += count;
+    }
+  }
 }
 
 void f2()
 {
-    printf("===================================================================================\n");
-    printf(" Q2. Count occurrences of a character (Normal Function Call)\n");
-    printf("===================================================================================\n\n");
+  printf("\n*===================== Using Normal Function Call =====================*\n");
+  printf("Q2: Count total duplicate elements in array\n\n");
+  int arr[10] = {1, 3, 7, 8, 8, 3, 3, 8, 8, 1};
+  int *ptr = arr;
 
-    char str[100] = "Hello World";
-    char *ptr = str;
-    char ch = 'l';
+  printf("Before Sorting: ");
+  for (int i = 0; i < 10; i++)
+    printf("%d ", *(arr + i));
 
-    int count = occurrences(ptr, ch);
+  shortArray(ptr, 10);
 
-    printf("String: \"%s\"\n", str);
-    printf("Character '%c' occurs %d times\n", ch, count);
+  printf("\nAfter Sorting:  ");
+  for (int i = 0; i < 10; i++)
+    printf("%d ", *(arr + i));
 
-    printf("\n===================================================================================\n");
-    printf("                              END OF PROGRAM 2                                        \n");
-    printf("===================================================================================\n\n\n");
+  countDuplicate(ptr, 10);
+
+  printf("\n*----------------------------- END OF Q2 -----------------------------*\n\n\n");
 }
 
 void f2_()
 {
-    printf("===================================================================================\n");
-    printf(" Q2.1 Count occurrences of a character (Pointer Function Call)\n");
-    printf("===================================================================================\n\n");
+  printf("\n*===================== Using Pointer Function Call ===================*\n");
+  printf("Q2.1: Count duplicates using function pointers\n\n");
 
-    char str[100] = "Hello World";
-    char *ptr = str;
-    char ch = 'l';
+  int arr[10] = {1, 7, 2, 5, 6, 7, 2, 9, 2, 0};
+  int *ptr = arr;
 
-    int (*fptr)(char *, char) = &occurrences;
-    int count = fptr(ptr, ch);
+  printf("Before Sorting: ");
+  for (int i = 0; i < 10; i++)
+    printf("%d ", *(arr + i));
 
-    printf("String: \"%s\"\n", str);
-    printf("Character '%c' occurs %d times\n", ch, count);
+  int (*fptr1)(int *, int) = &shortArray;
+  fptr1(ptr, 10);
 
-    printf("\n===================================================================================\n");
-    printf("                              END OF PROGRAM 2.1                                      \n");
-    printf("===================================================================================\n\n\n");
+  printf("\nAfter Sorting:  ");
+  for (int i = 0; i < 10; i++)
+    printf("%d ", *(arr + i));
+
+  void (*fptr2)(int *, int) = &countDuplicate;
+  fptr2(ptr, 10);
+
+  printf("\n*---------------------------- END OF Q2.1 ----------------------------*\n\n\n");
 }
 
-int vowels(char *ptr, char *v)
+// -------------------- Q3: Print Unique Elements --------------------
+void countUnique(int *ptr, int size)
 {
-    int i, j, count = 0;
-    for (i = 0; *(ptr + i) != '\0'; i++)
+  int counted = 0;
+  for (int i = 0; i < size; i++)
+  {
+    int count = 0;
+    for (int j = 0; j < size; j++)
     {
-        for (j = 0; *(v + j) != '\0'; j++)
-        {
-            if (*(ptr + i) == *(v + j))
-            {
-                count++;
-            }
-        }
+      if (i != j && *(ptr + i) == *(ptr + j))
+      {
+        count++;
+        break;
+      }
     }
-    return count;
+    if (count == 0)
+    {
+      if (!counted)
+        printf("\n");
+      printf("%d is unique\n", *(ptr + i));
+    }
+    counted = 1;
+  }
 }
 
 void f3()
 {
-    printf("===================================================================================\n");
-    printf(" Q3. Count vowels in a string (Normal Function Call)\n");
-    printf("===================================================================================\n\n");
+  printf("\n*===================== Using Normal Function Call =====================*\n");
+  printf("Q3: Print all unique elements in array\n\n");
 
-    char str[50] = "Hello World";
-    char v[50] = "aeiouAEIOU";
+  int arr[10] = {1, 7, 3, 5, 6, 3, 2, 2, 2, 1};
+  int *ptr = arr;
 
-    char *ptr = str;
-    char *vptr = v;
+  printf("Before Sorting: ");
+  for (int i = 0; i < 10; i++)
+    printf("%d ", *(arr + i));
 
-    int count = vowels(ptr, vptr);
+  shortArray(ptr, 10);
 
-    printf("String: \"%s\"\n", ptr);
-    printf("Number of vowels: %d\n", count);
+  printf("\nAfter Sorting:  ");
+  for (int i = 0; i < 10; i++)
+    printf("%d ", *(arr + i));
 
-    printf("\n===================================================================================\n");
-    printf("                          END OF PROGRAM 3                                            \n");
-    printf("===================================================================================\n\n\n");
+  printf("\n");
+  countUnique(ptr, 10);
+
+  printf("\n*----------------------------- END OF Q3 -----------------------------*\n\n\n");
 }
 
 void f3_()
 {
-    printf("===================================================================================\n");
-    printf(" Q3.1 Count vowels in a string (Pointer Function Call)\n");
-    printf("===================================================================================\n\n");
+  printf("\n*===================== Using Pointer Function Call ===================*\n");
+  printf("Q3.1: Print unique elements using function pointers\n\n");
 
-    char str[50] = "Hello World";
-    char v[50] = "aeiouAEIOU";
+  int arr[10] = {1, 7, 3, 5, 6, 4, 2, 7, 2, 6};
+  int *ptr = arr;
 
-    char *ptr = str;
-    char *vptr = v;
+  printf("Before Sorting: ");
+  for (int i = 0; i < 10; i++)
+    printf("%d ", *(arr + i));
 
-    int (*fptr)(char *, char *) = &vowels;
-    int count = (fptr)(ptr, vptr);
+  int (*fptr1)(int *, int) = &shortArray;
+  fptr1(ptr, 10);
 
-    printf("String: \"%s\"\n", ptr);
-    printf("Number of vowels: %d\n", count);
+  printf("\nAfter Sorting:  ");
+  for (int i = 0; i < 10; i++)
+    printf("%d ", *(arr + i));
 
-    printf("\n===================================================================================\n");
-    printf("                          END OF PROGRAM 3.1                                          \n");
-    printf("===================================================================================\n\n\n");
+  void (*fptr2)(int *, int) = &countUnique;
+  fptr2(ptr, 10);
+
+  printf("\n*---------------------------- END OF Q3.1 ----------------------------*\n\n\n");
 }
 
-int spaces(char *ptr)
+// -------------------- Q4: Merge Two Arrays --------------------
+void mergeArray(int *ptr1, int *ptr2, int size1, int size2)
 {
-    int count = 0;
-    for (int i = 0; ptr[i] != '\0'; i++)
-        if (ptr[i] == ' ')
-            count++;
-    return count;
+  int temp[size1 + size2];
+  int i, j;
+
+  for (i = 0; i < size1; i++)
+    temp[i] = *(ptr1 + i);
+
+  for (j = 0; j < size2; j++)
+    temp[i + j] = *(ptr2 + j);
+
+  printf("\nAfter Merging: ");
+  for (i = 0; i < size1 + size2; i++)
+    printf("%d ", temp[i]);
+
+  shortArray(temp, size1 + size2);
+
+  printf("\nAfter Sorting:  ");
+  for (i = 0; i < size1 + size2; i++)
+    printf("%d ", temp[i]);
 }
 
 void f4()
 {
-    printf("===================================================================================\n");
-    printf(" Q4. Count spaces in a string (Normal Function Call)\n");
-    printf("===================================================================================\n\n");
+  printf("\n*===================== Using Normal Function Call =====================*\n");
+  printf("Q4: Merge two arrays and sort them\n\n");
 
-    char str[100] = "Hello World";
-    char *ptr = str;
+  int arr1[3] = {1, 7, 3};
+  int arr2[3] = {11, 72, 33};
 
-    int count = spaces(ptr);
+  mergeArray(arr1, arr2, 3, 3);
 
-    printf("String: \"%s\"\n", str);
-    printf("Number of spaces: %d\n", count);
-
-    printf("\n===================================================================================\n");
-    printf("                              END OF PROGRAM 4                                        \n");
-    printf("===================================================================================\n\n\n");
+  printf("\n*----------------------------- END OF Q4 -----------------------------*\n\n\n");
 }
 
 void f4_()
 {
-    printf("===================================================================================\n");
-    printf(" Q4.1 Count spaces in a string (Pointer Function Call)\n");
-    printf("===================================================================================\n\n");
+  printf("\n*===================== Using Pointer Function Call ===================*\n");
+  printf("Q4.1: Merge two arrays using function pointer\n\n");
 
-    char str[100] = "Hello World";
-    char *ptr = str;
+  int arr1[3] = {1, 3, 6};
+  int arr2[3] = {4, 2, 7};
 
-    int (*fptr)(char *) = &spaces;
-    int count = fptr(ptr);
+  void (*fptr)(int *, int *, int, int) = &mergeArray;
+  fptr(arr1, arr2, 3, 3);
 
-    printf("String: \"%s\"\n", str);
-    printf("Number of spaces: %d\n", count);
-
-    printf("\n===================================================================================\n");
-    printf("                              END OF PROGRAM 4.1                                      \n");
-    printf("===================================================================================\n\n\n");
+  printf("\n*---------------------------- END OF Q4.1 ----------------------------*\n\n\n");
 }
 
-void convertIntoUpper(char *ptr)
+// -------------------- Q5: Count Frequency --------------------
+void countFrequency(int *ptr, int size)
 {
-    for (int i = 0; ptr[i] != '\0'; i++)
-        if (ptr[i] >= 'a' && ptr[i] <= 'z')
-            ptr[i] -= 32;
+  printf("\nElement   Frequency\n");
+  printf("-------   ---------\n");
+
+  for (int i = 0; i < size;)
+  {
+    int count = 1;
+    int j;
+    for (j = i + 1; j < size && *(ptr + j) == *(ptr + i); j++)
+      count++;
+
+    printf("   %d\t     %d\n", *(ptr + i), count);
+    i = j;
+  }
 }
 
 void f5()
 {
-    printf("===================================================================================\n");
-    printf(" Q5. Convert string to uppercase (Normal Function Call)\n");
-    printf("===================================================================================\n\n");
+  printf("\n*===================== Using Normal Function Call =====================*\n");
+  printf("Q5: Count frequency of each element\n\n");
 
-    char str[100] = "Hello World";
-    char *ptr = str;
+  int arr[10] = {1, 7, 3, 5, 6, 3, 2, 2, 2, 1};
 
-    convertIntoUpper(ptr);
+  shortArray(arr, 10);
+  countFrequency(arr, 10);
 
-    printf("Uppercase string: \"%s\"\n", str);
-
-    printf("\n===================================================================================\n");
-    printf("                              END OF PROGRAM 5                                        \n");
-    printf("===================================================================================\n\n\n");
+  printf("\n*----------------------------- END OF Q5 -----------------------------*\n\n\n");
 }
 
 void f5_()
 {
-    printf("===================================================================================\n");
-    printf(" Q5.1 Convert string to uppercase (Pointer Function Call)\n");
-    printf("===================================================================================\n\n");
+  printf("\n*===================== Using Pointer Function Call ===================*\n");
+  printf("Q5.1: Count frequency using function pointers\n\n");
 
-    char str[100] = "Hello World";
-    char *ptr = str;
+  int arr[10] = {1, 7, 3, 5, 6, 3, 2, 2, 2, 1};
 
-    void (*fptr)(char *) = &convertIntoUpper;
-    fptr(ptr);
+  int (*fptr1)(int *, int) = &shortArray;
+  fptr1(arr, 10);
 
-    printf("Uppercase string: \"%s\"\n", str);
+  void (*fptr2)(int *, int) = &countFrequency;
+  fptr2(arr, 10);
 
-    printf("\n===================================================================================\n");
-    printf("                              END OF PROGRAM 5.1                                      \n");
-    printf("===================================================================================\n\n\n");
+  printf("\n*---------------------------- END OF Q5.1 ----------------------------*\n\n\n");
 }
 
+// -------------------- Main Function --------------------
 int main()
 {
-    f1();
-    f1_();
-    f2();
-    f2_();
-    f3();
-    f3_();
-    f4();
-    f4_();
-    f5();
-    f5_();
+  f1();
+  f1_();
+  f2();
+  f2_();
+  f3();
+  f3_();
+  f4();
+  f4_();
+  f5();
+  f5_();
 
-    return 0;
+  return 0;
 }
